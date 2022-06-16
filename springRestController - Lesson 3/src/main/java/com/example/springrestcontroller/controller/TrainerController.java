8f64ccb00d6eba734cbc19ec3d4f68c6211877ce
hoang032001 <hoang032001@gmail.com>
2022-06-16 14:46:54 +0700

    optimal controller

--- springRestController - Lesson 3/src/main/java/com/example/springrestcontroller/controller/TrainerController.java
+++ springRestController - Lesson 3/src/main/java/com/example/springrestcontroller/controller/TrainerController.java
@@ -1,15 +1,11 @@
 package com.example.springrestcontroller.controller;
 
 import com.example.springrestcontroller.dto.UpdateTrainerRequest;
-import com.example.springrestcontroller.model.Trainer;
-import com.example.springrestcontroller.repository.TrainerRepository;
 import com.example.springrestcontroller.service.ITrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/trainers")
@@ -21,21 +17,13 @@ public class TrainerController {
     //JSON: { "id":? , "newName":"?new_horse_name?" }
     @PutMapping("/update")
     public ResponseEntity updateTrainer(@RequestBody UpdateTrainerRequest request){
-        try{
-            return new ResponseEntity<>(iTrainerService.updateTrainer(request), HttpStatus.OK);
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Error Exception");
-        }
+        return iTrainerService.updateTrainer(request);
     }
 
     //Post Man: DELETE -> http://localhost:8080/trainers/delete/?
     @DeleteMapping("/delete/{id}")
     public ResponseEntity deleteTrainer(@PathVariable("id") Integer id){
-        try{
-                return new ResponseEntity<>(iTrainerService.deleteTrainer(id), HttpStatus.OK);
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Error Exception");
-        }
+        return iTrainerService.deleteTrainer(id);
     }
 
 
